56f55a89a51b883c9446315ccf36f8ac00673dbc
galulex <alex@active-bridge.com>
2016-06-21 11:22:48 +0300

    Add passenger configs

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,3 +6,4 @@ set :repo_url, 'git@github.com:galulex/monitor.git'
 set :deploy_to, '/home/ubuntu'
 
 set :linked_files, fetch(:linked_files, []).push('config/app.yml')
+set :linked_dirs, fetch(:linked_dirs, []).push('tmp')
