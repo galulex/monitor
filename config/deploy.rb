aadd32c7587bb57e897fe3b245ab6362bd21504d
galulex <alex@active-bridge.com>
2016-06-21 10:05:37 +0300

    add linked file config

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,3 +4,5 @@ set :application, 'monitor'
 set :repo_url, 'git@github.com:galulex/monitor.git'
 
 set :deploy_to, '/home/ubuntu'
+
+set :linked_files, fetch(:linked_files, []).push('config/app.yml')
